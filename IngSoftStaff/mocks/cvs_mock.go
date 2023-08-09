6328f4fa7de5bb808500c7fdbe088adde90af6ae
gamousquesORT <mousques@ort.edu.uy>
2023-08-09 18:07:34 -0300

    mock csv Reader

--- IngSoftStaff/mocks/cvs_mock.go
+++ IngSoftStaff/mocks/cvs_mock.go
@@ -1,2 +1,17 @@
 package mocks
 
+import (
+    "bufio"
+    "encoding/csv"
+    "fmt"
+    "io"
+    "os"
+
+    "golang.org/x/text/encoding/charmap"
+)
+
+type CSVReader interface {
+    Read() ([]string, error)
+    ReadAll() ([][]string, error)
+}
+
